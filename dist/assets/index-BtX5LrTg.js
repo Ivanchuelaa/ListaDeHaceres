b92d38e0215abf02f618fb8dc6d0ff6498f28a33
ivan <ivangiron777@gmail.com>
2025-08-24 20:30:34 -0300

    Proyecto

---
+++ dist/assets/index-BtX5LrTg.js
@@ -0,0 +1,64 @@
+(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const o of document.querySelectorAll('link[rel="modulepreload"]'))i(o);new MutationObserver(o=>{for(const n of o)if(n.type==="childList")for(const u of n.addedNodes)u.tagName==="LINK"&&u.rel==="modulepreload"&&i(u)}).observe(document,{childList:!0,subtree:!0});function c(o){const n={};return o.integrity&&(n.integrity=o.integrity),o.referrerPolicy&&(n.referrerPolicy=o.referrerPolicy),o.crossOrigin==="use-credentials"?n.credentials="include":o.crossOrigin==="anonymous"?n.credentials="omit":n.credentials="same-origin",n}function i(o){if(o.ep)return;o.ep=!0;const n=c(o);fetch(o.href,n)}})();const L=`<section class="todoapp">\r
+    <header class="header">\r
+        <h1>Tareas</h1>\r
+        <input id="new-todo-input" class="new-todo" placeholder="¿Qué necesitas hacer?" autofocus>\r
+    </header>\r
+    \r
+    <!-- This section should be hidden by default and shown when there are todos -->\r
+    <section class="main">\r
+        <input id="toggle-all" class="toggle-all" type="checkbox">\r
+        <label for="toggle-all">Mark all as complete</label>\r
+        <ul class="todo-list">\r
+            <!-- These are here just to show the structure of the list items -->\r
+            <!-- List items should get the class "editing" when editing and "completed" when marked as completed -->\r
+            <!-- <li class="completed" data-id="abc">\r
+                <div class="view">\r
+                    <input class="toggle" type="checkbox" checked>\r
+                    <label>Probar JavaScript</label>\r
+                    <button class="destroy"></button>\r
+                </div>\r
+                <input class="edit" value="Create a TodoMVC template">\r
+            </li> -->\r
+            <!-- <li>\r
+                <div class="view">\r
+                    <input class="toggle" type="checkbox">\r
+                    <label>Comprar un unicornio</label>\r
+                    <button class="destroy"></button>\r
+                </div>\r
+                <input class="edit" value="Rule the web">\r
+            </li> -->\r
+        </ul>\r
+    </section>\r
+\r
+    <!-- This footer should hidden by default and shown when there are todos -->\r
+    <footer class="footer">\r
+        <!-- This should be "0 items left" by default -->\r
+        <span class="todo-count"><strong id="pending-count">0</strong> pendiente(s)</span>\r
+        <!-- Remove this if you don't implement routing -->\r
+        <ul class="filters">\r
+            <li>\r
+                <a class="selected filtro" class="selected" href="#/">Todos</a>\r
+            </li>\r
+            <li>\r
+                <a class="filtro" href="#/active">Pendientes</a>\r
+            </li>\r
+            <li>\r
+                <a class="filtro" href="#/completed">Completados</a>\r
+            </li>\r
+        </ul>\r
+        <!-- Hidden if no completed items are left ↓ -->\r
+        <button class="clear-completed">Borrar completados</button>\r
+    </footer>\r
+</section>\r
+\r
+\r
+<footer class="info">\r
+    <p>Creado por <a href="https://github.com/Ivanchuelaa">Ivanchuelaa</a></p>\r
+</footer>`,r=[];for(let e=0;e<256;++e)r.push((e+256).toString(16).slice(1));function C(e,t=0){return(r[e[t+0]]+r[e[t+1]]+r[e[t+2]]+r[e[t+3]]+"-"+r[e[t+4]]+r[e[t+5]]+"-"+r[e[t+6]]+r[e[t+7]]+"-"+r[e[t+8]]+r[e[t+9]]+"-"+r[e[t+10]]+r[e[t+11]]+r[e[t+12]]+r[e[t+13]]+r[e[t+14]]+r[e[t+15]]).toLowerCase()}let w;const S=new Uint8Array(16);function E(){if(!w){if(typeof crypto>"u"||!crypto.getRandomValues)throw new Error("crypto.getRandomValues() not supported. See https://github.com/uuidjs/uuid#getrandomvalues-not-supported");w=crypto.getRandomValues.bind(crypto)}return w(S)}const A=typeof crypto<"u"&&crypto.randomUUID&&crypto.randomUUID.bind(crypto),T={randomUUID:A};function I(e,t,c){if(T.randomUUID&&!e)return T.randomUUID();e=e||{};const i=e.random??e.rng?.()??E();if(i.length<16)throw new Error("Random bytes length must be >= 16");return i[6]=i[6]&15|64,i[8]=i[8]&63|128,C(i)}class g{constructor(t){this.id=I(),this.description=t,this.done=!1,this.createdAt=new Date}}const a={All:"all",Completed:"Completed",Pending:"Pending"},l={todos:[new g(" Ir al gimnasio"),new g(" Lavar los platos"),new g(" Comprar frutas"),new g(" Hacer las actividades de la uni"),new g(" Actualizar proyecto")],filter:a.All},P=()=>{v(),console.log("InitStore🥶")},v=()=>{if(!localStorage.getItem("state"))return;const{todos:e=[],filter:t=a.All}=JSON.parse(localStorage.getItem("state"));l.todos=e,l.filter=t},f=()=>{localStorage.setItem("state",JSON.stringify(l))},k=(e=a.All)=>{switch(e){case a.All:return[...l.todos];case a.Completed:return l.todos.filter(t=>t.done);case a.Pending:return l.todos.filter(t=>!t.done);default:throw new Error(`Option ${e} is not valid`)}},U=e=>{if(!e)throw new Error("Description is required");l.todos.push(new g(e)),f()},x=e=>{l.todos=l.todos.map(t=>(t.id===e&&(t.done=!t.done),t)),f()},D=e=>{l.todos=l.todos.filter(t=>t.id!==e),f()},O=()=>{l.todos=l.todos.filter(e=>!e.done),f()},q=(e=a.All)=>{l.filter=e,f()},F=()=>l.filter,d={addTodo:U,deleteCompleted:O,deleteTodo:D,getCurrentFilter:F,initStore:P,loadStore:v,setFilter:q,toggleTodo:x,getTodos:k},M=e=>{if(!e)throw new Error("A TODO object is required");const{done:t,description:c,id:i}=e,o=`
+                <div class="view">
+                    <input class="toggle" type="checkbox" ${t?"checked":""}>
+                    <label>${c}</label>
+                    <button class="destroy"></button>
+                </div>
+                <input class="edit" value="Create a TodoMVC template"
+    `,n=document.createElement("li");return n.innerHTML=o,n.setAttribute("data-id",i),e.done&&n.classList.add("completed"),n};let h;const H=(e,t=[])=>{if(h||(h=document.querySelector(e)),!h)throw new Error(`Element ${e} not found`);h.innerHTML="",t.forEach(c=>{h.append(M(c))})};let b;const N=e=>{if(b||(b=document.querySelector(e)),!b)throw new Error(`Element ${e} not found`);b.innerHTML=d.getTodos(a.Pending).length},m={ClearCompletedButton:".clear-completed",TodoList:".todo-list",NewTodoInput:"#new-todo-input",TodoFilters:".filtro",PendingCountLabel:"#pending-count"},R=e=>{const t=()=>{const s=d.getTodos(d.getCurrentFilter());H(m.TodoList,s),c()},c=()=>{N(m.PendingCountLabel)};(()=>{const s=document.createElement("div");s.innerHTML=L,document.querySelector(e).append(s),t()})();const i=document.querySelector(m.NewTodoInput),o=document.querySelector(m.TodoList),n=document.querySelector(m.ClearCompletedButton),u=document.querySelectorAll(m.TodoFilters);i.addEventListener("keyup",s=>{s.keyCode===13&&s.target.value.trim().length!==0&&(d.addTodo(s.target.value),t(),s.target.value="")}),o.addEventListener("click",s=>{const p=s.target.closest("[data-id]");d.toggleTodo(p.getAttribute("data-id")),t()}),o.addEventListener("click",s=>{const p=s.target.className==="destroy",y=s.target.closest("[data-id]");!y||!p||(d.deleteTodo(y.getAttribute("data-id")),t())}),n.addEventListener("click",()=>{d.deleteCompleted(),t()}),u.forEach(s=>{s.addEventListener("click",p=>{switch(u.forEach(y=>y.classList.remove("selected")),p.target.classList.add("selected"),p.target.text){case"Todos":d.setFilter(a.All);break;case"Pendientes":d.setFilter(a.Pending);break;case"Completados":d.setFilter(a.Completed);break}t()})})};d.initStore();R("#app");
